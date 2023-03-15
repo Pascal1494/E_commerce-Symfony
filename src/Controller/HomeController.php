525e4729d619d626964565d757c2e86f2d8a1a30
pascal494 <discokilly@laposte.net>
2023-03-15 17:42:07 +0100

    add Object in product's table

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,20 +2,28 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
 use App\Repository\ProductRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function homepage(ProductRepository $productRepository): Response
+    public function homepage(EntityManagerInterface $em): Response
     {
-        
+        $product = new Product;
 
+        $product->setName('table en plastique')
+            ->setPrice(3000)
+            ->setSlug('table-en-plastique');
 
+        $em->persist($product);
+        $em->flush();
 
+        dd($product);
 
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
