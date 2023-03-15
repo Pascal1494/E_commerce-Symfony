4efbc215d5bdd58a1f36ee9a66f467669680b37e
pascal494 <discokilly@laposte.net>
2023-03-15 18:16:09 +0100

    Add fixtures's  & Faker 's bundle

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,24 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\Entity\Product;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        for ($p = 0; $p < 100; $p++) {
+            $product = new Product;
+            $product
+                ->setName("Produit n°$p")
+                ->setPrice(mt_rand(100, 200))
+                ->setSlug("produit-n-$p");
+
+            $manager->persist($product);
+            
+        }
 
         $manager->flush();
     }
