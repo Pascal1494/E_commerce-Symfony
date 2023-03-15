6430e9ea79e945caeaf7a50b5da5bbcc4c672196
pascal494 <discokilly@laposte.net>
2023-03-15 19:47:30 +0100

    Add faker-provider-collection for the real name in BDD

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,23 +2,29 @@
 
 namespace App\DataFixtures;
 
+use Faker\Factory;
 use App\Entity\Product;
+use Liior\Faker\Prices;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+// use Doctrine\DBAL\Driver\IBMDB2\Exception\Factory;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
+        $faker = Factory::create('fr-FR');
+        $faker->addProvider(new \Liior\Faker\Prices($faker));
+        $faker->addProvider(new \Bezhanov\Faker\Provider\Commerce($faker));
+
         for ($p = 0; $p < 100; $p++) {
             $product = new Product;
             $product
-                ->setName("Produit n°$p")
-                ->setPrice(mt_rand(100, 200))
-                ->setSlug("produit-n-$p");
+                ->setName($faker->productName)
+                ->setPrice($faker->price(4000, 20000))
+                ->setSlug($faker->slug());
 
             $manager->persist($product);
-            
         }
 
         $manager->flush();
