e314649554e20557181f4406f6e33868f8264dd9
pascal494 <discokilly@laposte.net>
2023-03-15 17:22:38 +0100

    add personal function in Product's Entity

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +10,14 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function homepage(): Response
+    public function homepage(ProductRepository $productRepository): Response
     {
+        $product = $productRepository->findAll();
+        dump($product);
+
+
+
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
