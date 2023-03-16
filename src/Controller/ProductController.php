7144a2f109275371fae18e1195169baae94534ef
pascal494 <discokilly@laposte.net>
2023-03-16 18:20:27 +0100

    Create Formulaire withot bundle  and add boostrap's theme in YAML Folder

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,13 +3,25 @@
 namespace App\Controller;
 
 use App\Entity\Category;
+use App\Entity\Product;
 use App\Repository\CategoryRepository;
 use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class ProductController extends AbstractController
 {
@@ -48,6 +60,64 @@ class ProductController extends AbstractController
         return $this->render('product/show.html.twig', [
             'product' => $product,
             // 'urlGenerator' => $urlGenerator
+        ]);
+    }
+
+    #[Route('/admin/product/create', name: 'product_create')]
+    public function create(FormFactoryInterface $factory, Request $request, SluggerInterface $slugger, EntityManagerInterface $em)
+    {
+        // dd($request);
+        $builder = $factory->createBuilder(FormType::class, null, [
+            'data_class' => Product::class
+        ]);
+        $builder->add('name', TextType::class, [
+            'label' => 'Le nom du produit',
+            'attr' => ['placeholder' => 'Tapez le nom de votre produit']
+        ])
+            ->add('shortDescription', TextareaType::class, [
+                'label' => 'La description rapide du produit',
+                'attr' => ['placeholder' => 'Tapez une brève decription de votre produit mais qui envoie du steack !']
+            ])
+            ->add('price', MoneyType::class, [
+                'label' => 'Le prix de vente de votre produit',
+                'attr' => ['placeholder' => 'Tapez votre prix de vente de votre produit TTC et en €']
+            ])
+            ->add('mainPicture', UrlType::class, [
+                'label' => 'L\'image de votre produit',
+                'attr' => ['placeholder' => 'Sauvegarder l\'image de votre produit']
+            ])
+            ->add('category', EntityType::class, [
+                'label' => 'La catégorie de votre produit',
+                'attr' => [],
+                'placeholder' => '-- Choisir la catégorie de votre produit --',
+                'class' => Category::class,
+                'choice_label' => function (Category $category) {
+                    return strtoupper($category->getName());
+                }
+
+            ]);
+        $form = $builder->getForm();
+
+        $form->handleRequest($request);
+        // $data = $form->getData();
+        // dd($data);
+        if (
+            $form->isSubmitted() && $form->isValid()
+        ) {
+            $product = $form->getData();
+            $product->setSlug(strtolower($slugger->slug($product->getName())));
+
+            $em->persist($product);
+            $em->flush();
+            // dd($product);
+        }
+
+        $formView = $form->createView();
+
+        // dd($form);
+
+        return $this->render('product/create.html.twig', [
+            'formView' => $formView
         ]);
     }
 }
