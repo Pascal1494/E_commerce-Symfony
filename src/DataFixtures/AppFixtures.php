d8365e7897f2576851161e065c27fc15c3213018
pascal494 <discokilly@laposte.net>
2023-03-19 17:15:23 +0100

    Create User

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -6,19 +6,26 @@ use Faker\Factory;
 use App\Entity\Product;
 use Liior\Faker\Prices;
 use App\Entity\Category;
+use App\Entity\User;
 use Bluemmd\Faker\PicsumPhotosProvider;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\String\Slugger\SluggerInterface;
 
 // use Doctrine\DBAL\Driver\IBMDB2\Exception\Factory;
 
 class AppFixtures extends Fixture
 {
+    protected $slugger;
+    protected $encoder;
+
     public function __construct(
-        SluggerInterface $slugger
+        SluggerInterface $slugger,
+        UserPasswordHasherInterface $encoder
     ) {
         $this->slugger = $slugger;
+        $this->encoder = $encoder;
     }
     public function load(ObjectManager $manager): void
     {
@@ -26,6 +33,31 @@ class AppFixtures extends Fixture
         $faker->addProvider(new \Liior\Faker\Prices($faker));
         $faker->addProvider(new \Bezhanov\Faker\Provider\Commerce($faker));
         $faker->addProvider(new \Bluemmb\Faker\PicsumPhotosProvider($faker));
+
+        $admin = new User;
+
+        $hash = $this->encoder->hashPassword($admin, "password");
+
+        $admin->setEmail("admin@gmail.com")
+            ->setPassword($hash)
+            ->setFullname("Admin")
+            ->setRoles(['ROLE_ADMIN']);
+
+        $manager->persist($admin);
+
+
+        for ($u = 0; $u < 5; $u++) {
+            $user = new User();
+
+            $hash = $this->encoder->hashPassword($user, "password");
+
+
+            $user->setEmail("user$u@gmail.com")
+                ->setFullname($faker->name())
+                ->setPassword($hash);
+
+            $manager->persist($user);
+        }
 
         for ($c = 0; $c < 3; $c++) {
             $category = new Category;
